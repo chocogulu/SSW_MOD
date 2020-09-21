6e7428cb12bbd456d4cd619acc6e923dec3e72f9
YsikiShokurin <Ysiki.Shokurin@gmail.com>
2020-09-21 13:12:24 +0900

    アナキズム用の国旗を追加

--- The_Sun_Shining_on_the_World/gfx/flags/!FLAG_GENERATOR.py
+++ The_Sun_Shining_on_the_World/gfx/flags/!FLAG_GENERATOR.py
@@ -1,3 +1,4 @@
+import re
 import shutil
 import os
 
@@ -9,32 +10,26 @@ DIR_STRUCTURE = {"large": {"path": "", "template": "ANARCHISM汎用大.tga"},
                  "medium": {"path": "medium/", "template": "ANARCHISM汎用中.tga"},
                  "small": {"path": "small/", "template": "ANARCHISM汎用小.tga"}}
 
+# 国家タグ一覧を取得する
+tags_file = open(
+    "The_Sun_Shining_on_the_World/common/country_tags/00_countries.txt", encoding="utf-8")
+country_tags = set()
+for line in tags_file.readlines():
+    if re.match(r"\w{3}", line[:3]) != None:
+        country_tags.add(line[:3])  # 国家タグを追加
+
+print("Country tags", country_tags)
+
 for flag_type in DIR_STRUCTURE:
     # tgaファイルを格納するフォルダへのパス
     size_path = BASE_PATH + DIR_STRUCTURE[flag_type]["path"]
     template = DIR_STRUCTURE[flag_type]["template"]
     files = os.listdir(size_path)
-    country_tags = set()
 
-    for file in files:
-        if not os.path.isfile(os.path.join(
-                size_path, file)):  # フォルダを除外
-            continue
-        if not file.endswith(".tga"):  # tgaファイル以外を除外
-            continue
-        if file.startswith("ANARCHISM"):  # 特殊ファイルなので除外
-            continue
-        if file.startswith("Great"):  # 特殊ファイルなので除外
-            continue
-        if file.find("_") != -1:
-            country_tags.add(file.split("_")[0])  # _の前までをリストに追加
-        else:
-            country_tags.add(file.split(".")[0])  # .の前までをリストに追加
-    print(country_tags)
-
-    for tag in country_tags:
+    for tag in country_tags:  # タグごとにファイルを用意
         file_path = size_path + tag + "_anarchism.tga"
         if os.path.exists(file_path):
+            print(tag+"'s flag was skipped")
             continue  # 既にファイルが存在すれば
         shutil.copy(size_path+template, file_path)
         print(f"copied {tag}")
